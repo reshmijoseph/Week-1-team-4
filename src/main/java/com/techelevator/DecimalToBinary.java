fb58baeeaa8a3b55d853aca034e64cf64e549f33
Reshmi Joseph <reshmijosephk@gmail.com>
2021-09-21 00:11:45 -0400

    Decimals to Binary final commit

--- src/main/java/com/techelevator/DecimalToBinary.java
+++ src/main/java/com/techelevator/DecimalToBinary.java
@@ -6,31 +6,28 @@ import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
 
-		String userInput = input.nextLine();
+		String userInput = scanner.nextLine();
 
 		String[] decimalAsNumber = userInput.split(" ");
 		int[] binary = new int[11111111];
-		int i = 0;
-		int x;
 
 		for(String decimalNumber : decimalAsNumber){
 			int decimal = Integer.parseInt(decimalNumber);
 			System.out.print("\r\n"+ decimal + " in binary is ");
+			int i = 0;
 
 			while(decimal > 0) {
 				binary[i] = decimal % 2;
 //				System.out.print(binary[i] );
 				decimal = decimal / 2;
 				i++;
-
-				for (x = i - 1; x >= 0; x--) {
-					System.out.print(binary[x]);
-				}
 			}
+			for (int x = i - 1; x >= 0;x--)
+					System.out.print(binary[x]);
 		}
 	}
 }
